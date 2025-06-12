e473c36b613a32d3bd365ffbc6a1540b563720dc
boyfws <minkin.d.d@gmail.com>
2025-06-12 19:55:58 +0300

    raw_t naming fix

--- SDT/BaseSplitter.py
+++ SDT/BaseSplitter.py
@@ -7,7 +7,7 @@ class BaseSplitter(nn.Module):
     def __init__(self, input_dim: int, depth: int):
         super().__init__()
         self.raw_t = nn.Parameter(torch.empty(1, 2**depth - 1))
-        nn.init.xavier_uniform_(self.t)
+        nn.init.xavier_uniform_(self.raw_t)
 
         self.log_reg = nn.Linear(input_dim, 2**depth - 1)
 
